69eb74d143157f0efed55f1d1f3c5c1c65b58fc4
Jeffrey Kaditz <jeffrey.kaditz@gmail.com>
2012-02-29 12:14:48 -0800

    added erlang mode, has dependency on erlang being installed in /usr/local/lib and tools-2.6.6.6 which needs to be changed for each system

--- init.el
+++ init.el
@@ -9,6 +9,12 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/clojure-mode")
 (require 'clojure-mode)
 
+;Erlang Mode
+(setq load-path (cons  "/usr/local/lib/erlang/lib/tools-2.6.6.6/emacs" load-path))
+(setq erlang-root-dir "/usr/local/lib/erlang")
+(setq exec-path (cons "/usr/local/lib/erlang/bin" exec-path))
+(require 'erlang-start)
+
 ;; scala-mode
 (add-to-list 'load-path "~/.emacs.d/vendor/scala-mode")
 (require 'scala-mode)
